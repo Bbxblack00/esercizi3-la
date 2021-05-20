7a9f5a23f02fdf6f65538531578efab8b10dc7d4
B.C <benblog2000@gmail.com>
2021-05-20 09:24:40 +0200

    esercizio live

--- MyLaravel/app/Http/Controllers/ClassController.php
+++ MyLaravel/app/Http/Controllers/ClassController.php
@@ -4,6 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+class MyClass {
+    public $name;
+    public $value;
+    public function __construct($name, $value = -1) {
+        $this -> name = $name;
+        $this -> value = $value;
+        if ($value === -1) {
+            $this -> setRandomValue();
+        }
+    }
+    public function setRandomValue() {
+        $this -> value = rand(1, 1000);
+    }
+    public function getString() {
+        return "MyClass: " . $this -> name . " --> " . $this -> value;
+    }
+}
+
 class ClassController extends Controller
 {
     public function main() {
